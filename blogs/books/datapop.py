9d593c40363733ccba430bf04c9130f0a02a6344
PranjalJain24 <36255927+PranjalJain24@users.noreply.github.com>
2019-09-05 16:55:00 +0530

    Update datapop.py

--- blogs/books/datapop.py
+++ blogs/books/datapop.py
@@ -4,4 +4,4 @@ from books.models import *
 
 for i in range(10000):
 	author = Author.objects.create(first_name = namegenerator.gen(), last_name = namegenerator.gen(), author_name = namegenerator.gen())
-	book = Book.objects.create(title = namegenerator.gen(), authors = author, publishing_date = datetime.datetime.now().date(), isbn = namegenerator.gen())
+	post = Post.objects.create(title = namegenerator.gen(), authors = author, publishing_date = datetime.datetime.now().date(), isbn = namegenerator.gen())
